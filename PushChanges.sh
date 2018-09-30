272a29019239540154fab94656db808ca1ae477f
tests <test@examples.com>
2018-09-30 05:19:59 +0400

    Commit_my_2

---
+++ PushChanges.sh
@@ -0,0 +1,16 @@
+#git status
+
+rm -rf .git
+git init
+git add -A
+git commit -m Commit_my_2
+git remote add origin git@github.com:G4t/Trytestred.git
+git push --force origin master
+
+#"C:\Bitnami\redmine-3.4.6-1\servicerun.bat" START
+#sleep 180
+#start chrome.exe http://localhost/redmine
+#sleep 60
+#start chrome.exe http://localhost/redmine
+#sleep 90
+#start chrome.exe http://localhost/redmine
